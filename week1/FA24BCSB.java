fc360446790f0d695ae0034909639cd66145404c
ftvvv <ft7vvv@gmail.com>
2025-02-20 14:54:42 +0500

    week 1

---
+++ week1/FA24BCSB.java
@@ -0,0 +1,25 @@
+public class FA24BCSB {
+
+    public static void main(String[] args) {
+
+	int a = 25;
+	short b = 49;
+	long c = 99;
+	byte d = 122;
+	double  e = 91.2;
+	float f = 10000.44f;
+	boolean g = true;
+	char h = 'c';
+	String i = "Hafsa";
+
+        System.out.printf("Value in int: %d%n", a);
+        System.out.printf("Value in short: %d%n", b);
+        System.out.printf("Value in long: %d%n", c);
+        System.out.printf("Value in byte: %d%n", d);
+        System.out.printf("Value in double: %.2f%n", e);
+        System.out.printf("Value in float: %,.2f  %n", f);
+        System.out.printf("Value in boolean: %b%n", g);
+        System.out.printf("Value in char: %c%n", h);
+	System.out.printf("Value in string: %s%n", i);
+    }
+}
---
+++ week1/input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class input {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int I = 0;
+	System.out.println("enter num: ");
+	I = sc.nextInt();
+	System.out.printf("The value of interger is : %d",I);
+
+        
+    }
+}
